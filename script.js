c63b6e8ebf970e4f541c01eb13147e9cb5ab2dd4
raditya1ty <alifiansyahaji1106@gmail.com>
2024-11-14 15:30:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Mencegah halaman untuk reload
+// Fungsi untuk membuka sidebar
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main-content").style.marginLeft = "250px";
+}
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+// Fungsi untuk menutup sidebar
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main-content").style.marginLeft = "0";
+}
 
-    // Contoh validasi sederhana (username: admin, password: 1234)
-    if (username === 'admin' && password === 'Raditzto') {
-        // Redirect ke halaman dashboard
-        window.location.href = 'dashboard.html';
-    } else {
-        alert('Username atau password salah!');
-    }
-});
+// Mengambil elemen audio dari HTML
+const audio = document.getElementById("audio");
+
+// Fungsi untuk memainkan audio
+function playAudio() {
+    audio.play();
+}
+
+// Fungsi untuk menghentikan (pause) audio
+function pauseAudio() {
+    audio.pause();
+}
+
