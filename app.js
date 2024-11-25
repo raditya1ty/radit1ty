d81c749bf24e9933106c9702b3cd60d3a2a7e39f
raditya1ty <alifiansyahaji1106@gmail.com>
2024-11-25 17:35:45 +0700

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// Simple functionality to handle 'Add to Cart' button click
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Product added to cart');
-    });
-});
