16d9ac695bd6c7a6b6fb8d4e45be762262edbeb3
raditya1ty <alifiansyahaji1106@gmail.com>
2024-11-25 17:34:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,17 +9,3 @@ function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main-content").style.marginLeft = "0";
 }
-
-// Mengambil elemen audio dari HTML
-const audio = document.getElementById("audio");
-
-// Fungsi untuk memainkan audio
-function playAudio() {
-    audio.play();
-}
-
-// Fungsi untuk menghentikan (pause) audio
-function pauseAudio() {
-    audio.pause();
-}
-
